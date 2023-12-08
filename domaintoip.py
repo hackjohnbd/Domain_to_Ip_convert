b314ad80f6db52e0d69c062d9dcb09f85e7de18a
hackjohnbd <153279422+hackjohnbd@users.noreply.github.com>
2023-12-08 16:44:18 +0600

    Add files via upload

---
+++ domaintoip.py
@@ -0,0 +1,12 @@
+# domain to ip
+import socket
+import pyfiglet # color package
+from termcolor import colored #for color text
+print (colored(",,,,,,,,,,Domain,,,,,,,,To,,,,,,,Ip,,,,,,Conveter,,,,,,,,,,,",'red'))
+print (colored(",,,,,,,,,,Created,,,,,,,,By,,,,,,John,,,,,,,,,,,,,,",'yellow'))
+
+banner =colored(pyfiglet.figlet_format("Ip converter"), 'green') # use banner
+print(banner)
+domain_name = input ("input your domain here :")
+ip = socket.gethostbyname(domain_name)
+print("ip for {}	: {}".format(domain_name,ip))
